070530fd893d34c0d3ce9a9367145c54afdfebda
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2023-01-05 20:16:05 -0300

    type converter created

--- app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
 abstract class ToDoDatabase : RoomDatabase() {
 
     abstract fun toDoDao(): ToDoDao
