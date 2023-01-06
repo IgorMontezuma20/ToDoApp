ada601ff38dce94c8227d05cac19cfbde48b5f15
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2023-01-05 22:02:21 -0300

    inserting data in the db

--- app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
@@ -1,26 +1,102 @@
 package com.example.todoapp.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.example.todoapp.data.models.Priority
+import com.example.todoapp.data.models.ToDoData
+import com.example.todoapp.data.viewmodel.ToDoViewModel
+import com.example.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
 
         setHasOptionsMenu(true)
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.add_fragment_menu, menu)
+            }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.add_fragment_menu, menu)
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.menu_add) {
+                    insertDatatoDb()
+                } else if (menuItem.itemId == android.R.id.home) {
+                    requireActivity().onBackPressed()
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun insertDatatoDb() {
+        val mTitle = binding.titleEdt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEdt.text.toString()
+
+        val validation = verifyDataFromUser(mTitle, mDescription)
+        if(validation){
+            //Insert data to Database
+            val newData =ToDoData(
+                id= 0,
+                mTitle,
+                parsePriority(mPriority),
+                mDescription
+
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(
+                requireContext(),
+                "Adicionado com Sucesso!",
+                Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(
+                requireContext(),
+                "Por favor, preencha todos os campos.",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun verifyDataFromUser(title: String, description: String): Boolean{
+
+        return if(TextUtils.isEmpty(title) || TextUtils.isEmpty(description)){
+            false
+        }else !(title.isEmpty() || description.isEmpty())
+    }
+
+    private fun parsePriority(priority: String): Priority{
+        return when(priority){
+            "Alta Prioridade" -> {Priority.HIGH}
+            "Média Prioridade" -> {Priority.MEDIUM}
+            "Baixa Prioridade" -> {Priority.LOW}
+            else -> Priority.LOW
+        }
+
     }
 }
